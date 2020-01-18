79e348301918a0521e32f84439b144b242e85235
Janne Parviainen <janne.j.parviainen@gmail.com>
2020-01-18 20:05:26 +0200

    another typo

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   publicRuntimeConfig: {
     // Will be available on both server and client
     //BACKEND_URL: 'http://localhost:3010/api/v1',
-    BACKEND_URL: 'https://jayp-ecommerce.herokuapp.com',
+    BACKEND_URL: 'https://jayp-ecommerce.herokuapp.com/api/v1',
     BACKEND_APIKEY: 'g7se8ytgh3w89hg9a7f63asjkhfda32'
   }
 };
